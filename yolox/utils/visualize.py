2ad0acac0e1243a0c9b378b46d97b040ba9aa72c
dnth <dickson.neoh@gmail.com>
2022-04-20 12:10:09 +0800

    add viz plate

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -5,7 +5,42 @@
 import cv2
 import numpy as np
 
-__all__ = ["vis"]
+__all__ = ["vis", "vis_plate"]
+
+def vis_plate(img, boxes, scores, cls_ids, conf=0.5, class_names=None, plate_num=""):
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+        cls_id = int(cls_ids[i])
+        score = scores[i]
+        if score < conf:
+            continue
+        x0 = int(box[0])
+        y0 = int(box[1])
+        x1 = int(box[2])
+        y1 = int(box[3])
+
+        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+        text = '{}:{:.1f}% | '.format(class_names[cls_id], score * 100)
+        text+= plate_num
+        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+
+        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+        cv2.rectangle(
+            img,
+            (x0, y0 + 1),
+            (x0 + txt_size[0] + 150, y0 - int(3.5*txt_size[1])),
+            txt_bk_color,
+            -1
+        )
+        cv2.putText(img, text, (x0, y0 - txt_size[1]), font, 0.8, txt_color, thickness=2)
+        
+
+    return img
 
 
 def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
@@ -44,8 +79,8 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
 
 _COLORS = np.array(
     [
-        0.000, 0.447, 0.741,
         0.850, 0.325, 0.098,
+        0.000, 0.447, 0.741,
         0.929, 0.694, 0.125,
         0.494, 0.184, 0.556,
         0.466, 0.674, 0.188,
